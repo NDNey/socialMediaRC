1da4c9cdc21c3b42c5841e1e17cdc7661ad9c4bc
NDNey <ndavidney@gmail.com>
2021-11-06 14:45:24 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://project:project@cluster0.l6nuk.mongodb.net/socialMedia?retryWrites=true&w=majority', 
+    'dbName': 'socialMedia'
+};
